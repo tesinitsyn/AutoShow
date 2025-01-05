f86e04a44b4153c9794a696f94b70ccbab4a83b4
tesinitsyn <sitiev174@gmail.com>
2025-01-05 15:02:13 +0300

    Initial Commit

---
+++ AutoShow/AutoShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoShowApp.swift
+//  AutoShow
+//
+//  Created by Timofey Sinitsyn on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoShowTests/AutoShowTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoShowTests.swift
+//  AutoShowTests
+//
+//  Created by Timofey Sinitsyn on 1/5/25.
+//
+
+import Testing
+@testable import AutoShow
+
+struct AutoShowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
