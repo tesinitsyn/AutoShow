94742aa1861aac1d3b59e16ed63ae00b99fd817a
tesinitsyn <sitiev174@gmail.com>
2025-03-30 16:43:19 +0300

    Initial Commit

---
+++ AutoShow/AutoShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoShowApp.swift
+//  AutoShow
+//
+//  Created by Timofey Sinitsyn on 30.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AutoShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
